03be8353886499777a2b44aa7669ecaef53b0efc
monsterzack5 <monsterzack5@gmail.com>
2019-06-27 06:33:51 -0400

    DB Has its own module scope now

---
+++ src/lib/tools/db.ts
@@ -0,0 +1,45 @@
+import Sqlite from 'better-sqlite3';
+import { existsSync } from 'fs';
+import { exportFile } from './fileLoader';
+
+let database: Sqlite.Database;
+
+class Database {
+   private options: object = {};
+
+   public initDb(): boolean {
+      if (!existsSync(`./${process.env.dbFile}.db`)) {
+         console.error(new Error('Fatal Error: Database file not imported before init'));
+         return process.exit(3);
+      }
+      if (!database) {
+         if (process.env.NODE_ENV === 'dev') {
+            this.options = { verbose: console.log };
+         }
+         database = new Sqlite(`./${process.env.dbFile}.db`, this.options);
+      }
+      return true;
+   }
+
+   public get(): Sqlite.Database {
+      return database;
+   }
+}
+
+export const db = new Database();
+
+process.on('SIGINT', async (): Promise<void> => {
+   database.close();
+   await exportFile(`${process.env.dbFile}.db`);
+   console.log('aught interrupt signal, exiting!');
+   process.exit(0);
+});
+
+// only archive the database on sigterm
+process.on('SIGTERM', async (): Promise<void> => {
+   database.close();
+   await exportFile(`${process.env.dbFile}.db`);
+   await exportFile(`${process.env.dbFile}.db`, true);
+   console.log('Goodbye!');
+   process.exit(0);
+});
