f07892d6ce4fb3f0a11ebfc6716dbdbf8a8c21fb
monsterzack5 <monsterzack5@gmail.com>
2019-06-27 06:34:26 -0400

    Remove all require()'s, add typings

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,40 @@
-/* eslint-disable global-require */
-const fs = require('fs');
-const cron = require('node-cron');
-const request = require('request-promise-native');
-const http = require('http');
+// import cron from 'node-cron';
+import {
+   readFileSync, readdir, existsSync,
+   mkdirSync,
+} from 'fs';
+import request from 'request-promise-native';
+import http from 'http';
+import { Message, TextChannel } from 'discord.js';
+import { schedule } from 'node-cron';
 
-const spam = require('./lib/tools/antispam');
-const fileLoader = require('./lib/tools/fileLoader');
-const { bot } = require('./lib/tools/helper');
+import { checkAntiSpam } from './lib/tools/antispam';
+import { importFile, exportFile } from './lib/tools/fileLoader';
+import { bot } from './lib/tools/bot';
+import { checkRequired } from './lib/tools/required';
+import { db } from './lib/tools/db';
+
+if (!checkRequired()) throw new Error('Error! Enviorment Variables not set!');
 
 const port = process.env.PORT || 5010;
-let react;
-let chan;
-let bing;
-let db;
+let handleReact: (userID: string, url: string) => void;
+let updateChan: () => void;
+// let updateBing: () => void;
 
-// eslint-disable-next-line global-require
-if (fs.existsSync('.env')) require('dotenv').config();
+const commands = new Map();
+let prefix: string;
 
 // do different things in dev vs prod mode
 if (process.env.NODE_ENV === 'dev') {
    // use a different config options for dev mode
-   process.env.config_file = process.env.config_file_dev;
-   process.env.db_file = process.env.db_file_dev;
+   process.env.configFile = process.env.configFileDev;
+   process.env.dbFile = process.env.dbFileDev;
 
-   bot.login(process.env.discordtoken_dev);
+   bot.login(process.env.discordTokenDev);
 } else if (process.env.NODE_ENV === 'production') {
-   bot.login(process.env.discordtoken);
+   bot.login(process.env.discordToken);
    // ping our dyno every 15 minutes so heroku doesnt murder it
-   setInterval(() => {
+   setInterval((): void => {
       request({
          uri: 'https://catbitchbot.herokuapp.com',
       }).catch(console.error);
@@ -39,111 +46,149 @@ if (process.env.NODE_ENV === 'dev') {
 
 // create a http server that we can http get from our bot
 // so heroku doesnt disable the dyno from no traffic
-http.createServer((_req, res) => {
+http.createServer((_req, res): void => {
    res.end();
 }).listen(port);
 
-bot.on('ready', async () => {
+bot.on('ready', async (): Promise<void> => {
    console.log('Discord Client ready!');
 
    // this is to load Various files on boot and set runtime vars
    try {
-      await fileLoader.importFile(`${process.env.config_file}.json`);
-      await fileLoader.importFile(`${process.env.db_file}.db`);
+      await importFile(`${process.env.configFile}.json`);
+      await importFile(`${process.env.dbFile}.db`);
    } catch (error) {
-      console.log(`Error! Error importing (mandatory) boot files! \n${error}`);
+      console.error(new Error('Error! Error importing (mandatory) boot files!'));
       process.exit(1);
    }
 
-   // only load this when discord is ready
-   const { commands, initDB } = require('./lib/tools/helper');
-   initDB();
+   // only load everything else
+   db.initDb();
 
-   react = require('./lib/tools/react');
-   chan = require('./lib/tools/4chan');
-   bing = require('./lib/tools/bing');
-   // eslint-disable-next-line prefer-destructuring
-   db = require('./lib/tools/helper').db;
+   ({ handleReact } = await import('./lib/tools/react'));
+   ({ updateChan } = await import('./lib/tools/4chan'));
+   // ({ updateBing } = await import('./lib/tools/bing'));
 
-   const config = JSON.parse(fs.readFileSync(`./${process.env.config_file}.json`));
-   bot.prefix = config.prefix;
-   bot.commands = commands;
-
+   const config: ConfigOptions = JSON.parse(readFileSync(`./${process.env.configFile}.json`).toString());
+   process.env.prefix = config.prefix;
+   process.env.prefix = process.env.prefix as string;
 
    // set the game on boot
-   if (config.game_url === '') {
-      bot.user.setActivity(config.game, { type: config.game_state });
-   } else bot.user.setActivity(config.game, { type: config.game_state, url: config.game_url });
+   if (config.gameUrl === '') {
+      bot.user.setActivity(config.game, { type: config.gameState });
+   } else bot.user.setActivity(config.game, { type: config.gameState, url: config.gameUrl });
 
    // only import the command files AFTER discord.js says its ready
-   fs.readdir('./lib/', (err, files) => {
+   readdir('./dist/lib/', async (err, files): Promise<void> => {
       if (err) console.error(err);
       // this line only selects .js files, and adds them to command_files
-      const commandFiles = files.filter(f => f.includes('.js'));
-      for (const cmd of commandFiles) {
-         // eslint-disable-next-line import/no-dynamic-require, global-require
-         const props = require(`./lib/${cmd}`);
-         try {
-            bot.commands.set(props.help.name, props);
-         } catch (error) {
-            console.log('Error adding commands, file doesnt have name or help properties');
-            process.exit(1);
+      const commandFiles = files.filter(f => f.endsWith('.js'));
+      const cmdPromises: Promise<Command>[] = [];
+      for (const cmdFile of commandFiles) {
+         const importedCmd: Promise<Command> = import(`./lib/${cmdFile}`);
+         cmdPromises.push(importedCmd);
+      }
+      const test = await Promise.all(cmdPromises);
+      try {
+         for (const cmd of test) {
+            commands.set(cmd.help.name, cmd.default);
          }
+      } catch (error) {
+         console.log('Error adding commands, file doesnt have name or help properties');
+         process.exit(1);
       }
    });
 });
 
-bot.on('message', (message) => {
+bot.on('message', (message: Message): void => {
    if (message.author.bot) return;
    if (message.channel.type !== 'text'
-      && message.author.id !== process.env.bot_owner) {
+      && message.author.id !== process.env.botOwner) {
       message.react('🤔');
       return;
    }
-   const messageArguments = message.content.slice(bot.prefix.length).split(' ');
-   const command = messageArguments.shift();
-   if (message.content.startsWith(bot.prefix)) {
-      const cmdfunction = bot.commands.get(command);
+   if (message.content.startsWith(process.env.prefix as string)) {
+      const messageArguments = message.content.slice((process.env.prefix as string).length).split(' ');
+      const command = messageArguments.shift() as string;
+      const cmdfunction: (message: Message, args: string[]) => void | Promise<void> = commands.get(command);
       if (cmdfunction) {
-         const isSpam = spam.checkAntiSpam(message, command);
+         const isSpam = checkAntiSpam(message, command);
          if (!isSpam) {
-            cmdfunction.run(message, messageArguments, command, bot);
+            cmdfunction(message, messageArguments);
          } else {
             message.react('⏲');
          }
       }
    } else if (message.content.substring(1, 7) === 'prefix') {
-      message.channel.send(`My prefix is currently ${bot.prefix}!`);
+      message.channel.send(`My prefix is currently ${prefix}`);
    }
 });
 
 // this handle's reactions to our bot's messages
-bot.on('raw', async (data) => {
+bot.on('raw', async (data: RawReactData): Promise<void> => {
    if (data.t === 'MESSAGE_REACTION_ADD') {
-      const msg = await bot.channels.get(data.d.channel_id).fetchMessage(data.d.message_id);
+      const reactChannel = await bot.channels.get(data.d.channel_id) as TextChannel;
+      if (!reactChannel) {
+         return;
+      }
+      const msg = await reactChannel.fetchMessage(data.d.message_id);
       if (msg.author.id === bot.user.id
          && data.d.emoji.name === '🐱') {
-         react.handleReact(data.d.user_id, msg.embeds[0].image.url);
+         handleReact(data.d.user_id, msg.embeds[0].image.url);
       }
    }
 });
 
 // update the 4chan db, then backup the .db, then export the .db, every 5 minutes
-cron.schedule('*/5 * * * *', async () => {
-   if (!fs.existsSync('./tmp')) {
-      fs.mkdirSync('./tmp');
+schedule('*/5 * * * *', async (): Promise<void> => {
+   if (!existsSync('./tmp')) {
+      mkdirSync('./tmp');
    }
-
-   await chan.update();
+   await updateChan();
    try {
-      await db.backup(`./tmp/${process.env.db_file}.db`);
+      await db.get().backup(`./tmp/${process.env.dbFile}.db`);
    } catch (e) {
       console.error(`Error! Failed to backup the db!/n${e}`);
    }
-   await fileLoader.exportFile(`./tmp/${process.env.db_file}.db`);
+   await exportFile(`./tmp/${process.env.dbFile}.db`);
 });
 
 // updates our bing catboy db once a day at 1pm
-cron.schedule('0 13 * * *', () => {
-   bing.update();
-});
+// disabled until further notice
+// schedule('0 13 * * *', (): void => {
+//    updateBing();
+// });
+
+interface Command {
+   default(message: Message, args: string[]): void;
+   help: HelpProps;
+}
+
+interface HelpProps {
+   name: string;
+   help: string;
+}
+
+interface ConfigOptions {
+   prefix: string;
+   gameUrl: string;
+   game: string;
+   gameState: 'PLAYING' | 'STREAMING' | 'LISTENING' | 'WATCHING' | undefined;
+}
+
+interface RawReactData {
+   t: string;
+   s: number;
+   op: number;
+   d: {
+      user_id: string;
+      message_id: string;
+      guild_id: string;
+      channel_id: string;
+      emoji: {
+         name: string;
+         id: string | null;
+         animated: boolean;
+      };
+   };
+}
