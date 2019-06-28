68b8db150eef8e0bfc229df7c0502a515e65af50
monsterzack5 <monsterzack5@gmail.com>
2019-06-27 23:33:51 -0400

    Add command to give my discord id

---
+++ src/lib/support.ts
@@ -0,0 +1,10 @@
+import { Message } from 'discord.js';
+
+export default (message: Message): void => {
+   message.channel.send('If you need any support feel free to dm `-Zach#3260`');
+};
+
+export const help = {
+   name: 'support',
+   help: 'If you need any support :scream_cat:',
+};
