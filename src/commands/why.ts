bd18d798a1206369be2086c7f2dac13d3a0b9268
monsterzack5 <monsterzack5@gmail.com>
2019-08-01 23:13:30 -0400

    Add command to say why an imagine is in the database, and report info about said image

---
+++ src/commands/why.ts
@@ -0,0 +1,44 @@
+import { Message } from 'discord.js';
+import { db } from '../utils/db';
+
+const searchBing = db.prepare('SELECT * FROM bingcats WHERE url = ?');
+const searchBooru = db.prepare('SELECT * FROM boorucats WHERE url = ?');
+const searchChan = db.prepare('SELECT * FROM chancats WHERE no = ?');
+
+export default async (message: Message, args: string[]): Promise<void> => {
+   // if (message.author.id !== process.env.botOwner) {
+   //    message.react('❌');
+   //    return;
+   // }
+   if (args.length !== 1) {
+      const msg = await message.channel.send('Incorrect format, format should be : <prefix>why <url>') as Message;
+      msg.delete(3000);
+      return;
+   }
+
+   let url = args[0];
+   if (url.startsWith('`') && url.endsWith('`')) {
+      url = url.substring(1, url.length - 1);
+   }
+
+   if (url.startsWith('https://i.4cdn.org/cm/')) {
+      const no = url.substring(22, 35);
+      const chanCat = searchChan.get(no);
+      message.channel.send(`Cat is from 4chan, with No: ${chanCat.no} and Ext: ${chanCat.ext}`);
+      return;
+   }
+   const isBing = searchBing.get(url);
+   if (isBing) {
+      message.channel.send(`Cat is from bing, with ID: ${isBing.id} and Url: \`${isBing.url}\`\nHeight: ${isBing.height}, Width: ${isBing.width}\nPosted on: ${isBing.dateposted}\nAccentColor: ${isBing.color}\nName: ${isBing.name}`);
+      return;
+   }
+   const isBooru = searchBooru.get(url);
+   if (isBooru) {
+      message.channel.send(`Cat is from booru, with Url: \`${isBooru.url}\`\nSource: \`${isBooru.source}\`\nHeight: ${isBooru.height}, Width: ${isBooru.width}, Score: ${isBooru.score}\nTags: ${isBooru.tags}`);
+   }
+};
+
+export const help = {
+   name: 'why',
+   timeout: 300,
+};
