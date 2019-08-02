3dd6de52e13ac5a8fbff4bedb23ce51ff8c21403
monsterzack5 <monsterzack5@gmail.com>
2019-08-01 23:16:31 -0400

    Determine the odds of getting a booru, chan. and bing cat based on how many items they have in the DB, Major Refactoring

--- src/commands/catboy.ts
+++ src/commands/catboy.ts
@@ -1,61 +1,25 @@
-import { search as booru } from 'booru';
-import SearchResults from 'booru/dist/structures/SearchResults';
 import randomColor from 'randomcolor';
 import { Message } from 'discord.js';
 import { db } from '../utils/db';
-import { DiscordEmbedImageReply } from '../typings/interfaces';
+import { DiscordEmbedImageReply, RatioTuple } from '../typings/interfaces';
 
-
-// these statements get the length of the DB
-const bCount = db.prepare('SELECT COUNT(*) FROM bingcats');
-const cCount = db.prepare('SELECT COUNT(*) FROM chancats');
+// these statements get the number of entries for each cat table
+const countBing = db.prepare('SELECT COUNT(*) FROM bingcats');
+const countChan = db.prepare('SELECT COUNT(*) FROM chancats');
+const countBooru = db.prepare('SELECT COUNT(*) FROM boorucats');
 
 // these statements take a random number, and use it as the offset
 // to select a random cat
 const searchBing = db.prepare('SELECT * FROM bingcats LIMIT 1 OFFSET ?');
 const searchChan = db.prepare('SELECT * FROM chancats LIMIT 1 OFFSET ?');
+const searchBooru = db.prepare('SELECT * FROM boorucats LIMIT 1 OFFSET ?');
 
-// these vars hold the total items of both tables
-let bingCount = bCount.get()['COUNT(*)'];
-let chanCount = cCount.get()['COUNT(*)'];
+// these vars hold the total number of items of every table
+let bingCount = countBing.get()['COUNT(*)'];
+let chanCount = countChan.get()['COUNT(*)'];
+let booruCount = countBooru.get()['COUNT(*)'];
 
-// this searches the DB for filtered booru cats
-const searchFilteredById = db.prepare('SELECT * FROM filtered WHERE id = ?');
-
-// update vars that hold the total items of the tables every 5 minutes
-setInterval(() => {
-   bingCount = bCount.get()['COUNT(*)'];
-   chanCount = cCount.get()['COUNT(*)'];
-}, 600000);
-
-const sfwSites = [
-   {
-      site: 'danbooru.donmai.us',
-      search: [
-         'catboy', 'rating:safe',
-      ],
-   },
-   {
-      site: 'konachan.com',
-      search: [
-         'catboy', 'rating:safe',
-      ],
-   },
-   {
-      site: 'gelbooru.com',
-      search: [
-         'cat_boy', 'rating:safe',
-      ],
-   },
-   {
-      site: 'safebooru.org',
-      search: [
-         'cat_boy', 'rating:safe',
-      ],
-   },
-];
-const sitesLen = sfwSites.length;
-
+// this function create's a discord embed, from an image url and an optional source url
 function embedBuilder(source: string | undefined, url: string): DiscordEmbedImageReply {
    const color = parseInt((randomColor() as string).substring(1), 16);
    const description = !source ? '' : `[Source](${source})`;
@@ -70,43 +34,7 @@ function embedBuilder(source: string | undefined, url: string): DiscordEmbedImag
    };
 }
 
-
-async function booruCat(): Promise<DiscordEmbedImageReply | void> {
-   const randomNum = Math.floor(Math.random() * sitesLen);
-   let cat: SearchResults;
-
-   try {
-      cat = await booru(sfwSites[randomNum].site, sfwSites[randomNum].search, {
-         limit: 1,
-         random: true,
-      });
-   } catch (err) {
-      return console.error(err);
-   }
-   // this happens sometimes, i have no idea why
-   if (!cat[0].fileUrl) {
-      return booruCat();
-   }
-
-   // check if this catboy is filtered, if so return function recursively
-   const isFiltered = searchFilteredById.get(cat[0].fileUrl);
-   if (isFiltered) {
-      return booruCat();
-   }
-
-   const reply = embedBuilder(cat[0].source, cat[0].fileUrl);
-
-   return reply as DiscordEmbedImageReply;
-}
-
-function chanCat(): DiscordEmbedImageReply {
-   const random = Math.floor(Math.random() * chanCount);
-   const cat = searchChan.get(random);
-   const url = `https://i.4cdn.org/cm/${cat.no}${cat.ext}`;
-   const reply = embedBuilder('https://boards.4channel.org/cm/catalog#s=catboy', url);
-   return reply;
-}
-
+// returns a cat from bing
 function bingCat(): DiscordEmbedImageReply {
    const random = Math.floor(Math.random() * bingCount);
    const cat = searchBing.get(random);
@@ -115,20 +43,70 @@ function bingCat(): DiscordEmbedImageReply {
    return reply;
 }
 
-export async function getRandomCat(): Promise<DiscordEmbedImageReply> {
-   // picks a random number between 0 and X-1
-   const randomSearch = Math.floor(Math.random() * 50);
+// return a cat from 4chan
+function chanCat(): DiscordEmbedImageReply {
+   const random = Math.floor(Math.random() * chanCount);
+   const cat = searchChan.get(random);
+   const url = `https://i.4cdn.org/cm/${cat.no}${cat.ext}`;
+   const reply = embedBuilder('https://boards.4channel.org/cm/catalog#s=catboy', url);
+   return reply;
+}
+
+// return a cat from the booru's
+function booruCat(): DiscordEmbedImageReply {
+   const random = Math.floor(Math.random() * booruCount);
+   const cat = searchBooru.get(random);
+   const reply = embedBuilder(cat.source, cat.url);
+   return reply;
+}
+
+// this function generates the ratio's to use
+// when selecting what cat to send
+function getDbRatios(): RatioTuple {
+   const totalSize = bingCount + chanCount + booruCount;
+   const ratios: RatioTuple = [{
+      source: bingCat,
+      ratio: bingCount / totalSize,
+   },
+   {
+      source: chanCat,
+      ratio: chanCount / totalSize,
+   },
+   {
+      source: booruCat,
+      ratio: booruCount / totalSize,
+   }];
+   ratios.sort((a, b) => a.ratio - b.ratio);
+   return ratios;
+}
+
+// initalize dbRatios
+let dbRatios = getDbRatios();
+
+// this runs every 5 minutes, it updates the count's for every table
+// and refreshes the dbRatios
+setInterval(() => {
+   bingCount = countBing.get()['COUNT(*)'];
+   chanCount = countChan.get()['COUNT(*)'];
+   booruCount = countBooru.get()['COUNT(*)'];
+   dbRatios = getDbRatios();
+}, 600000);
+
+// gets a random cat from our db
+export function getRandomCat(): DiscordEmbedImageReply {
+   // picks a random number between 0 and 1
+   const randomSearch = Math.random();
    let reply;
 
-   // 0-3 for boorus, 4 to 10 for 4chan, 11 and up for bing
-   if (randomSearch < 10) reply = await booruCat() as DiscordEmbedImageReply;
-   if (randomSearch > 9 && randomSearch < 25) reply = chanCat();
-   if (randomSearch > 24) reply = bingCat();
+   if (randomSearch < dbRatios[0].ratio) reply = dbRatios[0].source();
+   if (randomSearch > dbRatios[0].ratio && randomSearch < dbRatios[1].ratio) reply = dbRatios[1].source();
+   if (randomSearch > dbRatios[1].ratio) reply = dbRatios[2].source();
    return reply as DiscordEmbedImageReply;
 }
 
+// this runs when you do <bot prefix>catboy
 export default async (message: Message): Promise<void> => {
-   const reply = await getRandomCat();
+   const reply = getRandomCat();
    message.channel.send(reply);
 };
 
