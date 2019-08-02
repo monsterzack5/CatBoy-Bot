d6010749682263a293238470c82e18f49f27482a
monsterzack5 <monsterzack5@gmail.com>
2019-08-01 23:17:18 -0400

    Filter by URL, add website filter with add or remove or list

--- src/commands/filter.ts
+++ src/commands/filter.ts
@@ -1,65 +1,59 @@
 import { Message } from 'discord.js';
+import { handleFilter } from '../utils/react';
 import { db } from '../utils/db';
 
-const insertFilter = db.prepare('INSERT INTO filtered (id, source) VALUES (?, ?)');
-
-const searchBing = db.prepare('SELECT * FROM bingcats WHERE id = ?');
-const searchChan = db.prepare('SELECT * FROM chancats WHERE no = ?');
-
-const deleteChan = db.prepare('DELETE FROM chancats WHERE no = ?');
-const deleteBing = db.prepare('DELETE FROM bingcats WHERE id = ?');
+const insertRegExp = db.prepare('INSERT OR REPLACE INTO filters (regex, source) VALUES (?, ?)');
+const searchFilter = db.prepare('SELECT * FROM filters WHERE regex = ?');
+const selectAllFilter = db.prepare('SELECT * FROM filters');
+const deleteFilter = db.prepare('DELETE FROM filters WHERE regex = ?');
 
 export default async (message: Message, args: string[]): Promise<void> => {
    if (message.author.id !== process.env.botOwner) {
       message.react('❌');
       return;
    }
-   if (args.length < 2) {
-      const msg = await message.channel.send('Err: not enough args') as Message;
-      msg.delete(3000);
+   if (!args.length) {
+      const msg = await message.channel.send('Incorrect format, format should be : <prefix>`filter <url>` or `filter add\\remove\\list [regex]`') as Message;
+      msg.delete(4500);
       return;
    }
-   switch (args[0].toLowerCase()) {
-      case 'bing': {
-         if (args[1].length !== 40) {
-            const msg = await message.channel.send('Err: bad id format for bing') as Message;
-            msg.delete(3000);
-            return;
-         }
-         const exists = searchBing.get(args[1]);
-         if (exists) {
-            insertFilter.run(args[1], 'bing');
-            deleteBing.run(args[1]);
-            message.react('✅');
-            return;
-         }
-         message.react('⁉');
-         break;
+   if (args[0] === 'add') {
+      let source = args[2];
+      if (args.length === 2) {
+         source = 'bing+booru';
+      } else if (source !== ('bing' || 'booru' || 'bing+booru')) {
+         message.channel.send('Error! source should either be `bing` or `booru` or `bing+booru`');
+         return;
       }
-
-      case 'chan': {
-         if (args[1].length !== 13) {
-            const msg = await message.channel.send('Err: bad id format for chan') as Message;
-            msg.delete(3000);
-            return;
-         }
-         const exists = searchChan.get(args[1]);
-         if (exists) {
-            insertFilter.run(args[1], 'chan');
-            deleteChan.run(args[1]);
-            message.react('✅');
-            return;
-         }
-         message.react('⁉');
-         break;
+      try {
+         RegExp(args[0]);
+      } catch (e) {
+         message.channel.send(`Error! That doesn't appear to be a valid RegExp!\n${e}`);
+         return;
       }
-
-      default: {
-         const msg = await message.channel.send('Err: bad source') as Message;
-         msg.delete(3000);
-         break;
-      }
+      insertRegExp.run(args[1], source);
+      message.channel.send('Added regex!');
+      return;
    }
+
+   if (args[0] === 'remove') {
+      const isRegex = searchFilter.get(args[1]);
+      if (isRegex) {
+         deleteFilter.run(args[1]);
+         message.channel.send(`Deleted filter ${isRegex.regex}`);
+         return;
+      }
+      message.channel.send('Error! I can\'t find that regex in my db :thinking:');
+      return;
+   }
+
+   if (args[0] === 'list') {
+      const allRegex = selectAllFilter.all();
+      const reply = allRegex.reduce((acc, val) => `${acc}\n\`${val.regex}, for tables: ${val.source}\``, '');
+      message.channel.send(`Currently active filters:${reply}`);
+      return;
+   }
+   handleFilter(args[0], message);
 };
 
 export const help = {
