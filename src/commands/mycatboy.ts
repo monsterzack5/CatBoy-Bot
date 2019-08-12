8bf0c310babae472861eea13b8d3f5295b60d5b4
monsterzack5 <monsterzack5@gmail.com>
2019-08-12 16:15:30 -0400

    mycatboy command can now work with @mentions

--- src/commands/mycatboy.ts
+++ src/commands/mycatboy.ts
@@ -5,7 +5,16 @@ import { db } from '../utils/db';
 const search = db.prepare('SELECT url FROM favorites WHERE uid = ?');
 
 export default async (message: Message): Promise<void> => {
-   const catboy = search.get(message.author.id);
+   const mention = message.mentions.members.first();
+   let { id } = message.author;
+   let msg: Message;
+   let username = '';
+
+   if (mention) {
+      ({ id } = mention.user);
+      ({ username } = mention.user);
+   }
+   const catboy = search.get(id);
    if (catboy) {
       const color = parseInt((randomColor() as string).substring(1), 16);
       message.channel.send({
@@ -18,7 +27,11 @@ export default async (message: Message): Promise<void> => {
       });
       return;
    }
-   const msg = await message.channel.send('You don\'t have a favorite catboy! React with :cat: to one of my messages to select your favorite catboy!') as Message;
+   if (!mention) {
+      msg = await message.channel.send('You don\'t have a favorite catboy! React with :cat: to one of my messages to select your favorite catboy!') as Message;
+   } else {
+      msg = await message.channel.send(`it looks like ${username} doesn't have a favorite catboy :crying_cat_face:`) as Message;
+   }
    msg.delete(8000);
 };
 
