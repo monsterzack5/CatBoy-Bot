a690eb8955e67efdb94ece9247c852c07e4a3c12
monsterzack5 <monsterzack5@gmail.com>
2019-08-23 06:54:16 -0400

    Move schema to file

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,21 +1,13 @@
 import Sqlite from 'better-sqlite3';
-import { existsSync } from 'fs';
+import { readFileSync, existsSync } from 'fs';
 import { exportFile } from './fileLoader';
 import { bot } from './bot';
 
 let database: Sqlite.Database;
 
 function execScema(db: Sqlite.Database): void {
-   db.exec('CREATE TABLE IF NOT EXISTS bingcats (id TEXT PRIMARY KEY, url TEXT NOT NULL, height INTEGER NOT NULL, width INTEGER NOT NULL, dateposted TEXT NOT NULL, name TEXT NOT NULL, color TEXT NOT NULL) WITHOUT ROWID');
-   db.exec('CREATE TABLE IF NOT EXISTS chancats (no INTEGER PRIMARY KEY, ext TEXT NOT NULL, height INTEGER NOT NULL, width INTEGER NOT NULL, filesize INTEGER NOT NULL, md5 TEXT NOT NULL, op INTEGER NOT NULL) WITHOUT ROWID');
-   db.exec('CREATE TABLE IF NOT EXISTS threads (no INTEGER PRIMARY KEY, status TEXT NOT NULL, lastmodified INTEGER) WITHOUT ROWID');
-   db.exec('CREATE TABLE IF NOT EXISTS favorites (uid TEXT PRIMARY KEY, url TEXT NOT NULL) WITHOUT ROWID');
-   db.exec('CREATE TABLE IF NOT EXISTS filtered (id TEXT PRIMARY KEY, source TEXT NOT NULL) WITHOUT ROWID');
-   db.exec('CREATE TABLE IF NOT EXISTS reports (url TEXT PRIMARY KEY, num INTEGER NOT NULL, source TEXT NOT NULL) WITHOUT ROWID');
-   db.exec('CREATE TABLE IF NOT EXISTS badurls (url TEXT PRIMARY KEY, source TEXT NOT NULL) WITHOUT ROWID');
-   db.exec('CREATE TABLE IF NOT EXISTS boorucats (url TEXT PRIMARY KEY, source TEXT, height INTEGER NOT NULL, width INTEGER NOT NULL, score INTEGER NOT NULL, tags TEXT) WITHOUT ROWID');
-   db.exec('CREATE TABLE IF NOT EXISTS filters (regex TEXT PRIMARY KEY, source TEXT NOT NULL)');
-   db.exec('CREATE TRIGGER IF NOT EXISTS remove_dead_links AFTER UPDATE OF status ON threads WHEN NEW.status = \'dead\' BEGIN DELETE FROM chancats WHERE op = OLD.no; END');
+   const schema = readFileSync('./schema.sql', 'utf-8');
+   db.exec(schema);
 }
 
 class Database {
