1cbaa7131a4834b313e80c0a85c9e0eab2226c73
monsterzack5 <monsterzack5@gmail.com>
2019-08-23 06:29:42 -0400

    Add sqlite trigger for 4chan.ts

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -7,14 +7,15 @@ let database: Sqlite.Database;
 
 function execScema(db: Sqlite.Database): void {
    db.exec('CREATE TABLE IF NOT EXISTS bingcats (id TEXT PRIMARY KEY, url TEXT NOT NULL, height INTEGER NOT NULL, width INTEGER NOT NULL, dateposted TEXT NOT NULL, name TEXT NOT NULL, color TEXT NOT NULL) WITHOUT ROWID');
-   db.exec('CREATE TABLE IF NOT EXISTS chancats (no TEXT PRIMARY KEY, ext TEXT NOT NULL) WITHOUT ROWID');
+   db.exec('CREATE TABLE IF NOT EXISTS chancats (no INTEGER PRIMARY KEY, ext TEXT NOT NULL, height INTEGER NOT NULL, width INTEGER NOT NULL, filesize INTEGER NOT NULL, md5 TEXT NOT NULL, op INTEGER NOT NULL) WITHOUT ROWID');
+   db.exec('CREATE TABLE IF NOT EXISTS threads (no INTEGER PRIMARY KEY, status TEXT NOT NULL, lastmodified INTEGER) WITHOUT ROWID');
    db.exec('CREATE TABLE IF NOT EXISTS favorites (uid TEXT PRIMARY KEY, url TEXT NOT NULL) WITHOUT ROWID');
    db.exec('CREATE TABLE IF NOT EXISTS filtered (id TEXT PRIMARY KEY, source TEXT NOT NULL) WITHOUT ROWID');
-   db.exec('CREATE TABLE IF NOT EXISTS threads (id INTEGER PRIMARY KEY, no TEXT NOT NULL) WITHOUT ROWID');
    db.exec('CREATE TABLE IF NOT EXISTS reports (url TEXT PRIMARY KEY, num INTEGER NOT NULL, source TEXT NOT NULL) WITHOUT ROWID');
    db.exec('CREATE TABLE IF NOT EXISTS badurls (url TEXT PRIMARY KEY, source TEXT NOT NULL) WITHOUT ROWID');
    db.exec('CREATE TABLE IF NOT EXISTS boorucats (url TEXT PRIMARY KEY, source TEXT, height INTEGER NOT NULL, width INTEGER NOT NULL, score INTEGER NOT NULL, tags TEXT) WITHOUT ROWID');
    db.exec('CREATE TABLE IF NOT EXISTS filters (regex TEXT PRIMARY KEY, source TEXT NOT NULL)');
+   db.exec('CREATE TRIGGER IF NOT EXISTS remove_dead_links AFTER UPDATE OF status ON threads WHEN NEW.status = \'dead\' BEGIN DELETE FROM chancats WHERE op = OLD.no; END');
 }
 
 class Database {
