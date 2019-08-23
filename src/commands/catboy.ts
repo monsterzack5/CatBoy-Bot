bba473fdbb0c401798ac679f435391fd33d34873
monsterzack5 <monsterzack5@gmail.com>
2019-08-23 06:29:16 -0400

    Moved db ratio's to its own namespace, can now update ratios via exported function

--- src/commands/catboy.ts
+++ src/commands/catboy.ts
@@ -1,12 +1,8 @@
 import randomColor from 'randomcolor';
 import { Message } from 'discord.js';
 import { db } from '../utils/db';
-import { DiscordEmbedImageReply, RatioTuple } from '../typings/interfaces';
-
-// these statements get the number of entries for each cat table
-const countBing = db.prepare('SELECT COUNT(*) FROM bingcats');
-const countChan = db.prepare('SELECT COUNT(*) FROM chancats');
-const countBooru = db.prepare('SELECT COUNT(*) FROM boorucats');
+import { getDbRatios } from '../utils/ratios';
+import { DiscordEmbedImageReply } from '../typings/interfaces';
 
 // these statements take a random number, and use it as the offset
 // to select a random cat
@@ -14,10 +10,22 @@ const searchBing = db.prepare('SELECT * FROM bingcats LIMIT 1 OFFSET ?');
 const searchChan = db.prepare('SELECT * FROM chancats LIMIT 1 OFFSET ?');
 const searchBooru = db.prepare('SELECT * FROM boorucats LIMIT 1 OFFSET ?');
 
-// these vars hold the total number of items of every table
-let bingCount = countBing.get()['COUNT(*)'];
-let chanCount = countChan.get()['COUNT(*)'];
-let booruCount = countBooru.get()['COUNT(*)'];
+// set dbRatios, all count variables on boot
+let {
+   dbRatios, bingCount, chanCount, booruCount,
+} = getDbRatios();
+
+// expose a function to update ratios
+export function updateRatios(): void {
+   ({
+      dbRatios, bingCount, chanCount, booruCount,
+   } = getDbRatios());
+}
+
+// runs every 5 minutes, updates the ratios
+setInterval(() => {
+   updateRatios();
+}, 600000);
 
 // this function create's a discord embed, from an image url and an optional source url
 function embedBuilder(source: string | undefined, url: string): DiscordEmbedImageReply {
@@ -35,7 +43,7 @@ function embedBuilder(source: string | undefined, url: string): DiscordEmbedImag
 }
 
 // returns a cat from bing
-function bingCat(): DiscordEmbedImageReply {
+export function bingCat(): DiscordEmbedImageReply {
    const random = Math.floor(Math.random() * bingCount);
    const cat = searchBing.get(random);
    const source = `https://www.bing.com/images/search?view=detailv2&id=${cat.id}`;
@@ -44,7 +52,7 @@ function bingCat(): DiscordEmbedImageReply {
 }
 
 // return a cat from 4chan
-function chanCat(): DiscordEmbedImageReply {
+export function chanCat(): DiscordEmbedImageReply {
    const random = Math.floor(Math.random() * chanCount);
    const cat = searchChan.get(random);
    const url = `https://i.4cdn.org/cm/${cat.no}${cat.ext}`;
@@ -53,44 +61,13 @@ function chanCat(): DiscordEmbedImageReply {
 }
 
 // return a cat from the booru's
-function booruCat(): DiscordEmbedImageReply {
+export function booruCat(): DiscordEmbedImageReply {
    const random = Math.floor(Math.random() * booruCount);
    const cat = searchBooru.get(random);
    const reply = embedBuilder(cat.source, cat.url);
    return reply;
 }
 
-// this function generates the ratio's to use
-// when selecting what cat to send
-function getDbRatios(): RatioTuple {
-   const totalSize = bingCount + chanCount + booruCount;
-   const ratios: RatioTuple = [{
-      source: bingCat,
-      ratio: bingCount / totalSize,
-   },
-   {
-      source: chanCat,
-      ratio: chanCount / totalSize,
-   },
-   {
-      source: booruCat,
-      ratio: booruCount / totalSize,
-   }];
-   ratios.sort((a, b) => a.ratio - b.ratio);
-   return ratios;
-}
-
-// initalize dbRatios
-let dbRatios = getDbRatios();
-
-// this runs every 5 minutes, it updates the count's for every table
-// and refreshes the dbRatios
-setInterval(() => {
-   bingCount = countBing.get()['COUNT(*)'];
-   chanCount = countChan.get()['COUNT(*)'];
-   booruCount = countBooru.get()['COUNT(*)'];
-   dbRatios = getDbRatios();
-}, 600000);
 
 // gets a random cat from our db
 export function getRandomCat(): DiscordEmbedImageReply {
@@ -104,7 +81,7 @@ export function getRandomCat(): DiscordEmbedImageReply {
    return reply as DiscordEmbedImageReply;
 }
 
-// this runs when you do <bot prefix>catboy
+// this runs when you do !catboy
 export default async (message: Message): Promise<void> => {
    const reply = getRandomCat();
    message.channel.send(reply);
