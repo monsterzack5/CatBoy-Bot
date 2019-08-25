8ab2b833fdeb49c5c73bcf60bc56977e6ff49375
monsterzack5 <monsterzack5@gmail.com>
2019-08-25 01:09:18 -0400

    Revert bba473f, add usage stats for users

--- src/commands/catboy.ts
+++ src/commands/catboy.ts
@@ -1,8 +1,7 @@
 import randomColor from 'randomcolor';
 import { Message } from 'discord.js';
 import { db } from '../utils/db';
-import { getDbRatios } from '../utils/ratios';
-import { DiscordEmbedImageReply } from '../typings/interfaces';
+import { DiscordEmbedImageReply, RatioTuple } from '../typings/interfaces';
 
 // these statements take a random number, and use it as the offset
 // to select a random cat
@@ -10,25 +9,24 @@ const searchBing = db.prepare('SELECT * FROM bingcats LIMIT 1 OFFSET ?');
 const searchChan = db.prepare('SELECT * FROM chancats LIMIT 1 OFFSET ?');
 const searchBooru = db.prepare('SELECT * FROM boorucats LIMIT 1 OFFSET ?');
 
-// set dbRatios, all count variables on boot
-let {
-   dbRatios, bingCount, chanCount, booruCount,
-} = getDbRatios();
+// Statments to get the db size for ratios
+const countBing = db.prepare('SELECT COUNT(*) FROM bingcats');
+const countChan = db.prepare('SELECT COUNT(*) FROM chancats');
+const countBooru = db.prepare('SELECT COUNT(*) FROM boorucats');
 
-// expose a function to update ratios
-export function updateRatios(): void {
-   ({
-      dbRatios, bingCount, chanCount, booruCount,
-   } = getDbRatios());
-}
+// count the catboys
+const updateBing = db.prepare('INSERT INTO userstats (uid, bing) VALUES(?, 1) ON CONFLICT(uid) DO UPDATE SET bing = bing + 1');
+const updateChan = db.prepare('INSERT INTO userstats (uid, chan) VALUES(?, 1) ON CONFLICT(uid) DO UPDATE SET chan = chan + 1');
+const updateBooru = db.prepare('INSERT INTO userstats (uid, booru) VALUES(?, 1) ON CONFLICT(uid) DO UPDATE SET booru = booru + 1');
 
-// runs every 5 minutes, updates the ratios
-setInterval(() => {
-   updateRatios();
-}, 600000);
+// declare some namespace variables
+let dbRatios: RatioTuple;
+let bingCount: number;
+let chanCount: number;
+let booruCount: number;
 
 // this function create's a discord embed, from an image url and an optional source url
-function embedBuilder(source: string | undefined, url: string): DiscordEmbedImageReply {
+function embedBuilder(url: string, source?: string): DiscordEmbedImageReply {
    const color = parseInt((randomColor() as string).substring(1), 16);
    const description = !source ? '' : `[Source](${source})`;
    return {
@@ -43,47 +41,74 @@ function embedBuilder(source: string | undefined, url: string): DiscordEmbedImag
 }
 
 // returns a cat from bing
-export function bingCat(): DiscordEmbedImageReply {
+function bingCat(uid: string): DiscordEmbedImageReply {
    const random = Math.floor(Math.random() * bingCount);
    const cat = searchBing.get(random);
    const source = `https://www.bing.com/images/search?view=detailv2&id=${cat.id}`;
-   const reply = embedBuilder(source, cat.url);
+   const reply = embedBuilder(cat.url, source);
+   updateBing.run(uid);
    return reply;
 }
 
 // return a cat from 4chan
-export function chanCat(): DiscordEmbedImageReply {
+function chanCat(uid: string): DiscordEmbedImageReply {
    const random = Math.floor(Math.random() * chanCount);
    const cat = searchChan.get(random);
    const url = `https://i.4cdn.org/cm/${cat.no}${cat.ext}`;
-   const reply = embedBuilder('https://boards.4channel.org/cm/catalog#s=catboy', url);
+   const reply = embedBuilder(url, 'https://boards.4channel.org/cm/catalog#s=catboy');
+   updateChan.run(uid);
    return reply;
 }
 
 // return a cat from the booru's
-export function booruCat(): DiscordEmbedImageReply {
+function booruCat(uid: string): DiscordEmbedImageReply {
    const random = Math.floor(Math.random() * booruCount);
    const cat = searchBooru.get(random);
-   const reply = embedBuilder(cat.source, cat.url);
+   const reply = embedBuilder(cat.url, cat.source);
+   updateBooru.run(uid);
    return reply;
 }
 
+// updates the ratio's for the db
+export function updateRatios(): void {
+   bingCount = countBing.get()['COUNT(*)'];
+   chanCount = countChan.get()['COUNT(*)'];
+   booruCount = countBooru.get()['COUNT(*)'];
+   const totalSize = bingCount + chanCount + booruCount;
+   dbRatios = [{
+      source: bingCat,
+      ratio: bingCount / totalSize,
+   },
+   {
+      source: chanCat,
+      ratio: chanCount / totalSize,
+   },
+   {
+      source: booruCat,
+      ratio: booruCount / totalSize,
+   }];
+   dbRatios.sort((a, b) => a.ratio - b.ratio);
+}
+updateRatios();
+
+// periodically update the ratios
+setInterval(updateRatios, 600000);
 
 // gets a random cat from our db
-export function getRandomCat(): DiscordEmbedImageReply {
+export function getRandomCat(uid: string): DiscordEmbedImageReply {
    // picks a random number between 0 and 1
    const randomSearch = Math.random();
    let reply;
 
-   if (randomSearch < dbRatios[0].ratio) reply = dbRatios[0].source();
-   if (randomSearch > dbRatios[0].ratio && randomSearch < dbRatios[1].ratio) reply = dbRatios[1].source();
-   if (randomSearch > dbRatios[1].ratio) reply = dbRatios[2].source();
+   if (randomSearch < dbRatios[0].ratio) reply = dbRatios[0].source(uid); else
+   if (randomSearch > dbRatios[0].ratio && randomSearch < dbRatios[1].ratio) reply = dbRatios[1].source(uid); else
+   if (randomSearch > dbRatios[1].ratio) reply = dbRatios[2].source(uid);
    return reply as DiscordEmbedImageReply;
 }
 
 // this runs when you do !catboy
 export default async (message: Message): Promise<void> => {
-   const reply = getRandomCat();
+   const reply = getRandomCat(message.author.id);
    message.channel.send(reply);
 };
 
--- src/commands/catboydump.ts
+++ src/commands/catboydump.ts
@@ -1,8 +1,9 @@
 import { Message } from 'discord.js';
 import { getRandomCat } from './catboy';
+import { DiscordEmbedImageReply } from '../typings/interfaces';
 
 export default async (message: Message): Promise<void> => {
-   const promiseCats = [1, 2, 3, 4, 5].map(getRandomCat);
+   const promiseCats: DiscordEmbedImageReply[] = [1, 2, 3, 4, 5].map(() => getRandomCat(message.author.id));
    const cats = await Promise.all(promiseCats);
    const reply = cats.reduce((acc, key) => `${acc}\n${key.embed.image.url}`, '');
    message.channel.send(reply);
