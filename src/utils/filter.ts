93ff6c9d3055c5b7da1e98c6d445657b1b9c83e0
monsterzack5 <monsterzack5@gmail.com>
2019-10-15 00:58:01 -0400

    Add filters.ts, a util function that takes a url and filters it from the db

---
+++ src/utils/filter.ts
@@ -0,0 +1,79 @@
+import { db } from './db';
+import { logger } from './logger';
+
+const insertFilter = db.prepare('INSERT INTO filtered (id, source) VALUES (?, ?)');
+
+const deleteChan = db.prepare('DELETE FROM chancats WHERE no = ?');
+const deleteBing = db.prepare('DELETE FROM bingcats WHERE id = ?');
+const deleteReport = db.prepare('DELETE FROM reports WHERE url = ?');
+const deleteBooru = db.prepare('DELETE FROM boorucats WHERE url = ?');
+
+const selectBing = db.prepare('SELECT * FROM bingcats WHERE url = ?');
+const selectFiltered = db.prepare('SELECT * FROM filtered WHERE id = ?');
+const selectBooru = db.prepare('SELECT * FROM boorucats WHERE url = ?');
+
+
+// FIXME: the fallthrough case of a url being bing+booru will
+// result in an sql unique error
+
+function filterChan(url: string): boolean {
+   if (url.startsWith('https://i.4cdn.org')) {
+      const postNumber = url.substring(22, 35);
+      const isAlreadyFiltered = selectFiltered.get(postNumber);
+      if (isAlreadyFiltered) {
+         deleteReport.run(url);
+         const wasDeleted = deleteChan.run(url);
+         return wasDeleted.changes > 0;
+      }
+      insertFilter.run(postNumber, 'chan');
+      deleteChan.run(postNumber);
+      deleteReport.run(url);
+      logger.log(`Filtered catboy from chan table with url: \`${url}\``);
+      return true;
+   }
+   return false;
+}
+
+function filterBing(url: string): boolean {
+   const isBing = selectBing.get(url);
+   if (isBing) {
+      const isAlreadyFiltered = selectFiltered.get(url);
+      if (isAlreadyFiltered) {
+         deleteReport.run(url);
+         const wasDeleted = deleteBing.run(url);
+         return wasDeleted.changes > 0;
+      }
+      insertFilter.run(url, 'bing');
+      deleteBing.run(url);
+      deleteReport.run(url);
+      logger.log(`Filtered catboy from bing table with url: \`${url}\``);
+      return true;
+   }
+   return false;
+}
+
+function filterBooru(url: string): boolean {
+   const isBooru = selectBooru.get(url);
+   if (isBooru) {
+      const isAlreadyFiltered = selectFiltered.get(url);
+      if (isAlreadyFiltered) {
+         deleteReport.run(url);
+         const wasDeleted = deleteBooru.run(url);
+         return wasDeleted.changes > 0;
+      }
+      insertFilter.run(url, 'booru');
+      deleteBooru.run(url);
+      deleteReport.run(url);
+      logger.log(`Filtered catboy from booru table with url: \`${url}\``);
+      return true;
+   }
+   return false;
+}
+
+
+export function filterUrl(url: string): boolean {
+   const didChanFilter = filterChan(url);
+   const didBingFilter = filterBing(url);
+   const didBooruFilter = filterBooru(url);
+   return (didChanFilter || didBingFilter || didBooruFilter);
+}
